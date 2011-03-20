65bf0cd9069c7bc2e1c9bc9874222aa08d0e04c1
Drew Schuster <dr3wster@gmail.com>
2011-03-20 04:02:55 -0400

    code cleanup

--- WaWA/WaWA.c
+++ WaWA/WaWA.c
@@ -1,11 +1,4 @@
-/******************************************************************************/
-/* BLINKY.C: LED Flasher                                                      */
-/******************************************************************************/
-/* This file is part of the uVision/ARM development tools.                    */
-/* Copyright (c) 2005-2006 Keil Software. All rights reserved.                */
-/* This software may only be used under the terms of a valid, current,        */
-/* end user licence from KEIL for a compatible version of KEIL software       */
-/* development tools. Nothing else gives you the right to use this software.  */
+/* WaWA.c : Wireless Audio System
 /******************************************************************************/
                   
 #include <stdio.h>
